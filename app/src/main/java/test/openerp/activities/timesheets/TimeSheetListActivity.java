9c6277871015c1e821748f365fa787af267e74fc
anshulpatro <anshulpatro@gmail.com>
2017-02-09 15:58:45 +0530

    Timesheet Layout

--- app/src/main/java/test/openerp/activities/timesheets/TimeSheetListActivity.java
+++ app/src/main/java/test/openerp/activities/timesheets/TimeSheetListActivity.java
@@ -1,45 +1,39 @@
 package test.openerp.activities.timesheets;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import test.openerp.R;
 
-public class TimesheetsActivity extends AppCompatActivity {
+public class TimeSheetListActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    String timesheetview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timesheets);
+        setContentView(R.layout.activity_time_sheet_list);
 
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Timesheets");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
-    @OnClick(R.id.bt_mytimesheet)
-    void onClickMyTimesheets(){
-        Intent i = new Intent(getApplicationContext(), TimeSheetListActivity.class);
-        i.putExtra("TIMESHEET","My Timesheet");
-        startActivity(i);
-    }
+        Intent intent = getIntent();
+        timesheetview = intent.getStringExtra("TIMESHEET");
+        getSupportActionBar().setTitle(timesheetview);
 
-    @OnClick(R.id.bt_teamtimesheet)
-    void onClickTeamTimesheets(){
-        Intent i = new Intent(getApplicationContext(), TimeSheetListActivity.class);
-        i.putExtra("TIMESHEET","Team Timesheet");
-        startActivity(i);
     }
 
     @Override
